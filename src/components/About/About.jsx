ee34c755d048d41f38a72ed7b7d84bd9f661c0c5
thebuilderekes <ekeopreberedugo1@gmail.com>
2023-06-25 23:25:30 +0100

    updated about section and hover link colors

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,133 +6,146 @@ import styled from "styled-components";
 import "animate.css";
 
 function About() {
-  return (
-    <Section
-      className="about-section"
-      id="about"
-    >
-      <img
-        src={cartoonImg}
-        alt="cartoon-like image of wuka"
-      />
-      <div className="card">
-        <h2 className="about-heading animate__animated animate__swing animate__infinite	infinite">
-          WHO AM I?
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum maiores
-          nam vitae magnam aliquid est accusamus itaque obcaecati corporis dicta
-          modi voluptatum quibusdam rerum provident, non tempore. Quasi eius
-          odio accusamus laboriosam ducimus aut autem aperiam. Labore iusto
-          voluptate maxime. Neque explicabo ex nesciunt obcaecati aliquam velit
-          ipsa quibusdam vero?
-        </p>
-      </div>
-    </Section>
-  );
+	return (
+		<Section className="about-section" id="about">
+			<img src={cartoonImg} alt="cartoon-like image of wuka" />
+			<div className="card">
+				<h2 className="about-heading animate__animated animate__swing animate__infinite	infinite">
+					ABOUT
+				</h2>
+				<p>
+					Introducing the mesmerizing melodies and soul-stirring compositions of
+					our talented musician. With the release of his highly anticipated
+					debut album, he invites listeners on an extraordinary musical journey
+					that transcends genres and captivates hearts. This groundbreaking
+					collection showcases his unique artistry, effortlessly blending
+					elements of pop, rock, and alternative, creating an unmistakable sound
+					that resonates with audiences worldwide. <br />
+					<br />
+					As an emerging artist, he is quickly becoming a sensation, gaining
+					momentum and admiration for his exceptional talent and undeniable
+					stage presence. His infectious energy and magnetic personality have
+					garnered a growing fan base, eagerly following his every move. From
+					sold-out shows in intimate venues to soaring chart positions, this
+					rising star is leaving an indelible mark on the music scene.
+					<br />
+					<br />
+					With each captivating performance and every heartfelt lyric, his star
+					power continues to shine brighter, firmly establishing him as a force
+					to be reckoned with in the industry. Stay tuned as this musical
+					virtuoso continues to soar to new heights, enchanting audiences with
+					his soulful voice, mesmerizing compositions, and an unwavering
+					dedication to his craft. With his debut album as a testament to his
+					undeniable talent, he is poised to make an enduring impact on the
+					world of music, captivating hearts and captivating the hearts and
+					minds of music lovers everywhere.
+				</p>
+			</div>
+		</Section>
+	);
 }
 
 export default About;
 
 const Section = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #b75a64;
-  padding: 5%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: #b75a64;
+	padding: 5%;
 
-  @media (max-width: 800px) {
-    padding: 18% 0;
-  }
+	@media (max-width: 800px) {
+		padding: 18% 0;
+	}
 
-  @media (max-width: 400px) {
-    padding: 15% 0;
-  }
+	@media (max-width: 400px) {
+		padding: 15% 0;
+	}
 
-  img {
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
+	img {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
 
-    @media (max-width: 800px) {
-      margin: 8% 0;
-    }
-    @media (max-width: 400px) {
-      width: 150px;
-      height: 150px;
-      margin: 5% 0;
-    }
-  }
+		@media (max-width: 800px) {
+			margin: 8% 0;
+		}
+		@media (max-width: 400px) {
+			width: 150px;
+			height: 150px;
+			margin: 5% 0;
+		}
+	}
 
-  .card {
-    border-radius: 16px;
-    background-color: #b4ab98;
-    box-shadow: 10px 10px 20px 0px rgba(99, 54, 58, 0.75);
-    -webkit-box-shadow: 10px 10px 20px 0px rgba(99, 54, 58, 0.75);
-    -moz-box-shadow: 10px 10px 20px 0px rgba(99, 54, 58, 0.75);
-    padding: 1% 5%;
+	.card {
+		border-radius: 16px;
+		background-color: #b4ab98;
+		box-shadow: 10px 10px 20px 0px rgba(99, 54, 58, 0.75);
+		-webkit-box-shadow: 10px 10px 20px 0px rgba(99, 54, 58, 0.75);
+		-moz-box-shadow: 10px 10px 20px 0px rgba(99, 54, 58, 0.75);
+		padding: 1% 5%;
 
-    @media (min-width: 700px) {
-      margin: 2% 15%;
-    }
+		@media (min-width: 700px) {
+			margin: 2% 15%;
+		}
 
-    @media (max-width: 699px) {
-      padding: 5% 10%;
-      margin: 3% 5%;
-    }
+		@media (max-width: 699px) {
+			padding: 5% 10%;
+			margin: 3% 5%;
+		}
 
-    h2 {
-      font-size: var(--large-fontSize);
-      text-align: center;
-      color: #ccddef;
-      background-color: var(--dark-color);
-      padding: 1% 5%;
-      margin: 2% 30%;
-      font-family: "Crimson Text";
-      font-weight: 700;
+		h2 {
+			font-size: var(--large-fontSize);
+			text-align: center;
+			color: #ccddef;
+			background-color: var(--dark-color);
+			padding: 1% 5%;
+			margin: 2% 30%;
+			font-family: "Crimson Text";
+			font-weight: 700;
 
-      @media (min-width: 1400px) {
-        margin: 2% 20%;
-      }
+			@media (min-width: 1400px) {
+				margin: 2% 20%;
+			}
 
-      @media (max-width: 900px) {
-        margin: 2% 25%;
-        padding: 3%;
-      }
+			@media (max-width: 900px) {
+				margin: 2% 25%;
+				padding: 3%;
+			}
 
-      @media (max-width: 600px) {
-        text-align: center;
-        color: #ccddef;
-        background-color: var(--dark-color);
-        padding: 1% 10%;
-        margin: 2% 23%;
-      }
+			@media (max-width: 600px) {
+				text-align: center;
+				color: #ccddef;
+				background-color: var(--dark-color);
+				padding: 1% 10%;
+				margin: 2% 23%;
+			}
 
-      @media (max-width: 500px) {
-        font-size: var(--medium-fontSize);
-        text-align: center;
-        color: #ccddef;
-        background-color: var(--dark-color);
-        padding: 1% 0%;
-        margin: 2% 20%;
-      }
-    }
+			@media (max-width: 500px) {
+				font-size: var(--medium-fontSize);
+				text-align: center;
+				color: #ccddef;
+				background-color: var(--dark-color);
+				padding: 1% 0%;
+				margin: 2% 20%;
+			}
+		}
 
-    p {
-      font-size: var(--medium-fontSize);
-      margin: 5% 0;
-      line-height: 1.5;
-      font-family: ${CrimsonWoff2400};
+		p {
+			font-size: var(--medium-fontSize);
+			margin: 5% 0;
+			line-height: 1.5;
+			font-family: ${CrimsonWoff2400};
 
-      @media (max-width: 520px) {
-        font-size: var(--default-fontSize);
-        margin: 5% 2%;
-      }
-    }
-  }
+			@media (max-width: 520px) {
+				font-size: var(--default-fontSize);
+				margin: 5% 2%;
+			}
+		}
+	}
 `;
 
 const Img = styled.img`
-  border-radius: 50%;
-  margin: 0 auto;
+	border-radius: 50%;
+	margin: 0 auto;
 `;
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,154 +5,152 @@ import "../../index.css";
 import bgImg from "../../assets/wuka-2.webp";
 
 function HeroSection() {
-  return (
-    <Section className="hero-section">
-      <div className="hero-section-text">
-        <h1 className=" animate__animated animate__fadeIn  animate__delay-1s">
-          WUKA
-        </h1>
-        <h2 className=" animate__animated animate__fadeIn  animate__delay-2s">
-          THE ALPHA
-        </h2>
-        <nav className=" animate__animated animate__fadeIn  animate__delay-3s">
-          <a
-            href="https://spacefly.lnk.to/rigel"
-            target="_blank"
-          >
-            Stream the Album here
-          </a>
-        </nav>
-      </div>
-    </Section>
-  );
+	return (
+		<Section className="hero-section">
+			<div className="hero-section-text">
+				<h1 className=" animate__animated animate__fadeIn  animate__delay-1s">
+					WUKA
+				</h1>
+				<h2 className=" animate__animated animate__fadeIn  animate__delay-2s">
+					THE ALPHA
+				</h2>
+				<nav className=" animate__animated animate__fadeIn  animate__delay-3s">
+					<a href="https://spacefly.lnk.to/rigel" target="_blank">
+						Stream the Album here
+					</a>
+				</nav>
+			</div>
+		</Section>
+	);
 }
 
 export default HeroSection;
 
 const Section = styled.section`
-  width: 100%;
-  height: 100vh;
-  background-image: url(${bgImg});
-  background-repeat: no-repeat;
-  background-size: cover;
-  font-family: "Crimson Text";
-  position: relative;
+	width: 100%;
+	height: 100vh;
+	background-image: url(${bgImg});
+	background-repeat: no-repeat;
+	background-size: cover;
+	font-family: "Crimson Text";
+	position: relative;
 
-  .hero-section-text {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 40%;
-    height: 40%;
-    padding: 5% 2rem;
-    text-align: center;
+	.hero-section-text {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 40%;
+		height: 40%;
+		padding: 5% 2rem;
+		text-align: center;
 
-    nav {
-      a {
-        font-size: var(--large-fontSize);
-        color: #ffffff;
-        background-color: #1a1212;
-        padding: 10px;
-        text-decoration: none;
-        &:hover {
-          color: #cf3259;
-        }
+		nav {
+			a {
+				font-size: var(--large-fontSize);
+				color: #ffffff;
+				background-color: #1a1212;
+				padding: 10px;
+				text-decoration: none;
+				&:hover {
+					color: #cf3259;
+					background-color: #999999;
+				}
 
-        @media (max-width: 500px) {
-          font-size: var(--medium-fontSize);
-        }
-      }
-    }
+				@media (max-width: 500px) {
+					font-size: var(--medium-fontSize);
+				}
+			}
+		}
 
-    @media (max-width: 800px) {
-      background-color: #ffffff86;
-      width: 100%;
-      height: 100vh;
-      padding: 25% 2rem;
-    }
+		@media (max-width: 800px) {
+			background-color: #ffffff86;
+			width: 100%;
+			height: 100vh;
+			padding: 25% 2rem;
+		}
 
-    @media (max-width: 400px) {
-      padding: 25% 1rem;
-    }
+		@media (max-width: 400px) {
+			padding: 25% 1rem;
+		}
 
-    h1 {
-      font-size: var(--extralarge-fontSize);
-      padding-top: 5%;
-      z-index: 10;
-      background-color: #1d1d1d;
-      -webkit-background-clip: text;
-      -moz-background-clip: text;
-      background-clip: text;
-      color: transparent;
-      text-shadow: #ffffff7e 3px 5px 1px;
+		h1 {
+			font-size: var(--extralarge-fontSize);
+			padding-top: 5%;
+			z-index: 10;
+			background-color: #1d1d1d;
+			-webkit-background-clip: text;
+			-moz-background-clip: text;
+			background-clip: text;
+			color: transparent;
+			text-shadow: #ffffff7e 3px 5px 1px;
 
-      @media (max-width: 800px) {
-        h1 {
-          font-size: var(--semilarge-fontSize);
-        }
-      }
+			@media (max-width: 800px) {
+				h1 {
+					font-size: var(--semilarge-fontSize);
+				}
+			}
 
-      @media (max-width: 400px) {
-        h1 {
-          font-size: var(--large-fontSize);
-        }
-      }
+			@media (max-width: 400px) {
+				h1 {
+					font-size: var(--large-fontSize);
+				}
+			}
 
-      @media (max-width: 360px) {
-        h1 {
-          font-size: var(--medium-fontSize);
-        }
-      }
-      span {
-        writing-mode: horizontal-tb;
-      }
-    }
+			@media (max-width: 360px) {
+				h1 {
+					font-size: var(--medium-fontSize);
+				}
+			}
+			span {
+				writing-mode: horizontal-tb;
+			}
+		}
 
-    h2 {
-      margin: 0 0 5% 0;
-      z-index: 10;
-      font-size: var(--large-fontSize);
-      background-color: #1d1d1d;
-      -webkit-background-clip: text;
-      -moz-background-clip: text;
-      background-clip: text;
-      color: transparent;
-      text-shadow: #ffffff7e 3px 5px 1px;
+		h2 {
+			margin: 0 0 5% 0;
+			z-index: 10;
+			font-size: var(--large-fontSize);
+			background-color: #1d1d1d;
+			-webkit-background-clip: text;
+			-moz-background-clip: text;
+			background-clip: text;
+			color: transparent;
+			text-shadow: #ffffff7e 3px 5px 1px;
 
-      a {
-        &:hover {
-          color: #cf3259;
-        }
-      }
-    }
+			a {
+				&:hover {
+					color: #cf3259;
+				}
+			}
+		}
 
-    ul {
-      li {
-        list-style: none;
-        padding: 10px 0;
+		ul {
+			li {
+				list-style: none;
+				padding: 10px 0;
 
-        a {
-          color: #cf3259;
-          text-decoration: none;
-          box-shadow: inset 0 0 0 0 #636363;
-          margin: 0 -0.25rem;
-          padding: 0 0.25rem;
-          transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-          font-size: 24px;
-          font-family: "Cinzel";
-          font-weight: 700;
-          transition: 1s all;
+				a {
+					color: #cf3259;
+					text-decoration: none;
+					box-shadow: inset 0 0 0 0 #636363;
+					margin: 0 -0.25rem;
+					padding: 0 0.25rem;
+					transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+					font-size: 24px;
+					font-family: "Cinzel";
+					font-weight: 700;
+					transition: 1s all;
 
-          @media (max-width: 400px) {
-            font-size: 16px;
-          }
-        }
+					@media (max-width: 400px) {
+						font-size: 16px;
+					}
+				}
 
-        a:hover {
-          box-shadow: inset 10px 0 10px 0 #636363;
-          color: #000000;
-        }
-      }
-    }
-  }
+				a:hover {
+					box-shadow: inset 10px 0 10px 0 #636363;
+					color: #000000;
+				}
+			}
+		}
+	}
 `;
