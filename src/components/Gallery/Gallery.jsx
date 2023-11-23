fe4b93c88944a2a9b71cfacb9fd7e80c2d2213c2
thebuilderekes <ekeopreberedugo1@gmail.com>
2023-11-23 10:46:51 +0100

    updated image fetching

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import "./gallery.css";
+import wukaphoto1 from "../../assets/img/wuka-gal1.jpg";
+import wukaphoto2 from "../../assets/img/wuka-gal2.jpg";
 
-const images = [
-	"./src/assets/img/wuka-gal1.jpg",
-	"./src/assets/img/wuka-gal2.jpg",
-];
+const images = [wukaphoto1, wukaphoto2];
 
 function Gallery() {
 	const [currentIndex, setCurrentIndex] = useState(0);
